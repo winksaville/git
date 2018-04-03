1f0c4ac807936e525f48b504e9d3f727561911b4
Wink Saville <wink@Winks-MacBook-Air.local>
2018-04-03 14:48:44 -0700

    Remove extra lines.

--- ci/run-build-and-tests.sh
+++ ci/run-build-and-tests.sh
@@ -10,8 +10,3 @@ ln -s "$cache_dir/.prove" t/.prove
 make --jobs=2
 cd t
 prove --timer --jobs 2 ./t001*.sh ./t0050*.sh ./t0204*.sh ./t9822*.sh
-#./t0050-filesystem.sh
-#./t0204-gettext-reencode-sanity.sh
-#./t9822-git-p4-path-encoding.sh
-
-rm t/.prove
